7093eed8c1e1f3285bb28b29f3654294858e304a
azz <azz@debian>
2017-02-27 22:25:05 +0300

    one day it will be so good ^^

--- source/eventsList.c
+++ source/eventsList.c
@@ -9,11 +9,7 @@
 #include "../yTimerService.h"
 
 eventItem_typedef pool[MAX_EVENTS];
-eventItem_typedef* eventsList = NULL;
-
-eventItem_typedef* elInit(){
-	return &(pool[0]);
-}
+eventItem_typedef** eventsList = NULL;
 
 eventItem_typedef* elGetBlockFromPool(eventItem_typedef* pool){
 	eventItem_typedef* retVal = (eventItem_typedef*)(void*)0;
@@ -35,39 +31,72 @@ void elReturnBlockToPool(eventItem_typedef* pool, eventItem_typedef* item){
 	item->time_ms = 0;
 }
 
-void elAddItem(eventItem_typedef* head, void* smHandle, sc_eventid evid, sc_integer time_ms, sc_boolean periodic){
+eventItem_typedef** elAddItem(eventItem_typedef** eventsList, void* smHandle, sc_eventid evid, sc_integer time_ms, sc_boolean periodic){
+
 	eventItem_typedef* tempEvent = elGetBlockFromPool(pool);
+	if(!tempEvent)
+	{
+		while(1);
+	}
+
 	tempEvent->evid = evid;
 	tempEvent->handle = smHandle;
 	tempEvent->periodic = periodic;
 	tempEvent->remainingTime_ms = time_ms;
 	tempEvent->time_ms = time_ms;
 	tempEvent->next = (eventItem_typedef*)0;
-	eventItem_typedef* tempHead = head;
+
+	if(eventsList == NULL){
+		return &tempEvent;
+	}
+
+	eventItem_typedef* tempHead = *eventsList;
+
 	while(tempHead->next){
 		tempHead = tempHead->next;
 	}
 	tempHead->next = tempEvent;
-	elSortByRemainingTime(head);
+	elSortByRemainingTime(eventsList);
+
+	if(*eventsList->evid == evid){
+		//update remaining time for all elements and change timer interrupt time and
+	}
 }
 
-void elRemoveByID(eventItem_typedef* head, sc_eventid evid){
-	eventItem_typedef* event = head;
+void elRemoveByID(eventItem_typedef** eventsList, sc_eventid evid){
+	if(*eventsList->evid == evid){
+		elRemoveFromTop(eventItem_typedef** eventsList);
+	}
+
+	eventItem_typedef* eventPrev = *eventsList;
+	eventItem_typedef* event = *eventsList->next;
+
 	while(event->next && (event->evid != evid)){
+		eventPrev = event;
 		event = event->next;
 	}
-	if(event){
-		event->allocated = false;
-	}
-	if(event == head){
-		head = head->next;
-	}
-}
-void elRemoveFromTop(eventItem_typedef* list){
-	eventItem_typedef* toDelete = list;
-	list = list->next;
-	elReturnBlockToPool(pool, toDelete);
+	eventPrev = event->next;
 
+	elReturnBlockToPool(pool, event);
+}
+
+void elRemoveFromTop(eventItem_typedef** eventsList){
+	void hwTimerStop();
+	uint32_t timerReduceVal = hwTimerGetTime();
+	eventItem_typedef* iter = (*eventsList)->next;
+	while(iter->next){
+		iter->remainingTime_ms -= timerReduceVal;
+	}
+	hwTimerSetTimer((*eventsList)->next->remainingTime_ms);
+	hwTimerStart();
+
+	eventItem_typedef* toDelete = *eventsList;
+	*eventsList = *eventsList->next;
+	elReturnBlockToPool(pool, toDelete);
+}
+
+void elSortByRemainingTime(eventItem_typedef** eventsList){
+	*eventsList = mergeSort(*eventsList);
 }
 
 static eventItem_typedef* bisectList(eventItem_typedef* head)
